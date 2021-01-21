b0e9ea61e92703c4725135ffe4e86ff90e848673
Kevin Joly <kevin.joly@armadeus.com>
2021-01-21 12:35:42 +0100

    awesome: change launcher icon

--- awesome/themes/default/theme.lua
+++ awesome/themes/default/theme.lua
@@ -90,7 +90,8 @@ theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
 theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
 theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
 
-theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
+-- theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
+theme.awesome_icon = "~/.config/awesome/themes/default/launcher.png"
 
 -- Define the icon theme for application icons. If not set then the icons 
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
